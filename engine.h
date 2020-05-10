027b77e650103caeab1b32aa543c3e1ed4c9e726
UldrichBruno <uldrich.bruno@seznam.cz>
2020-05-10 18:29:51 +0200

    3. etapa

---
+++ engine.h
@@ -0,0 +1,14 @@
+//
+// Created by Uzivatel on 10.05.2020.
+//
+
+#ifndef GRAM_SCHMIDT_ENGINE_H
+#define GRAM_SCHMIDT_ENGINE_H
+
+double dotProduct(struct matrix Matrix, struct vector  whichOneLeft, struct vector whichOneRight);
+double sum(struct matrix Matrix, struct vector whichOneGet, struct vector theOneBefore);
+double dotProduct(struct matrix Matrix, struct vector whichOneLeft, struct vector whichOneRight);
+struct vector Gram_Schmidt(struct matrix givenMatrix, struct vector insteadOf, struct vector theOneBefore);
+
+
+#endif //GRAM_SCHMIDT_ENGINE_H
